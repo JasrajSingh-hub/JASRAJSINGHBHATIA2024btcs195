36ccd5167a15eb5961db47861feef4031b4681b8
JasrajSingh-hub <jasujasraj123@gmail.com>
2026-07-27 16:38:14 +0530

    KHGKG

--- ass8/src/feature/contact/component/contact.jsx
+++ ass8/src/feature/contact/component/contact.jsx
@@ -1,11 +1,17 @@
 import Nav from "../../../shared/component/nav";
+import { useState } from "react";
 
 function Contact(){
-    function App() {
   const [value , setValue]= useState(" ");
+   const [value2 , setValue2]= useState(" ");
+    const [value3 , setValue3]= useState(" ");
+  const [v1 ,setv1]=useState(" ");
   const [v2 ,setv2]=useState(" ");
+  const [v3 ,setv3]=useState(" ");
   function handleClick() {
- setv2(value); 
+    setv1(value)
+    setv2(value2)
+    setv3(value3)
  }
 
 
@@ -13,14 +19,22 @@ function Contact(){
         <>
         <Nav />
          <div>
-            <input onChange={(e)=> setValue(e.target.value)}/>
-            <button onClick={()=>{handleClick()}}>idk</button>
+            <p>name</p>
+            <div className="flex gap-4">
+            <input className="border-2" onChange={(e)=> setValue(e.target.value)}/>
+            <input className="border-2" onChange={(e)=> setValue2(e.target.value2)}/>
+            <input className="border-2" onChange={(e)=> setValue3(e.target.value3)}/>
+            </div>
+            <button className="border-2"onClick={()=>{handleClick()}}>button</button>
+            <p>{v1}</p>
             <p>{v2}</p>
-            <p>{value}</p>
+            <p>{v3}</p>
+
+            
             </div>
  </>
 
     )
 }
-}
+
 export default Contact;
