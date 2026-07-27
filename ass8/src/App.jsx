cfc4603f15ef81e14dbea0b853bef1ae54a453d3
JasrajSingh-hub <jasujasraj123@gmail.com>
2026-07-27 14:29:49 +0530

    hheeeehehehe

--- ass8/src/App.jsx
+++ ass8/src/App.jsx
@@ -1,8 +1,18 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './feature/home/component/home';
+import About from './feature/about/component/about';
+import Contact from './feature/contact/component/contact';
 
 function App(){
   return(
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ ass8/src/feature/home/component/home.jsx
@@ -0,0 +1,16 @@
+function Home(){
+    return(
+        <>
+        <div>
+            <div>
+                <h1>
+                    Welcome
+                 </h1>
+                 <p>Hi my name is </p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home;
